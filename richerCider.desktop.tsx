90002d645abddb567588e20e38a9b76f7bb75e55
Nikolas Sturm <code@nsturm.me>
2024-07-09 19:48:14 +0200

    change id's for custom rpc clients

--- richerCider.desktop.tsx
+++ richerCider.desktop.tsx
@@ -21,10 +21,8 @@ import { Logger } from "@utils/Logger";
 import definePlugin from "@utils/types";
 import { Forms } from "@webpack/common";
 const appIds = [
-    "911790844204437504",
-    "886578863147192350",
-    "1020414178047041627",
-    "1032800329332445255"
+    "1036656884066820267",
+    "1036660041807310918"
 ];
 const logger = new Logger("richerCider");
 export default definePlugin({
